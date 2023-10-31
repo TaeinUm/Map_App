6ab78a22cc2ebed2102154052878e6826f3a1be2
JuyeeMyeong <juyee.ming@gmail.com>
2023-10-31 15:29:46 -0400

    #1 front-demo, x map
    front-end layout, without map & community page layout

---
+++ src/components/SignUp.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../App.css";
+
+function SignUp() {
+  return (
+    <div className="signup-page">
+      <div className="container">
+        <h2>TerraCanvas</h2>
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Username" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <div>
+            <input type="checkbox" id="terms" />
+            <label htmlFor="terms">
+              I agree with Dribbble's Terms of Service, Privacy Policy, and
+              default Notification Settings.
+            </label>
+          </div>
+          <button type="submit">Create Account</button>
+          <p>
+            Already have an account? <a href="/signin">Sign In</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
