e1627f749138202c88020c6607e540a56b0dd7d0
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-02 18:19:01 -0400

    #2 update header style (interactive style)

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,14 @@
-import "../styles/App.css";
+import React, { useState } from "react";
+import "../../styles/App.css";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
     <header className="hdr d-flex justify-content-between align-items-center">
       <div
@@ -11,12 +18,32 @@ function Header() {
       >
         TERRACANVAS
       </div>
+      <div
+        className={`hamburger-menu ${isOpen ? "rotate" : ""}`}
+        onClick={() => handleMenuClick()}
+      >
+        ☰
+      </div>
+      {isOpen && (
+        <div className="mobile-nav active">
+          <Link to="/">Home</Link>
+          <Link to="/map">Map</Link>
+          <Link to="/community">Community</Link>
+          <Link to="/termsconditions">Terms and Conditions</Link>
+          <Link to="/privacypolicy">Privacy Policy</Link>
+          <Link to="/contact">Contact</Link>
+          <div className="auth-buttons">
+            <Link to="/signin">Sign In</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        </div>
+      )}
       <nav className="navbar d-flex">
-        <a href="/">HOME</a>
-        <a href="/map">MAP</a>
-        <a href="/community">COMMUNITY</a>
+        <Link to="/">HOME</Link>
+        <Link to="/map">MAP</Link>
+        <Link to="/community">COMMUNITY</Link>
       </nav>
-      <div className="user-section d-flex algin-items-center">
+      <div className="user-section d-flex align-items-center">
         <button>
           <Link to="/signin">Sign In</Link>
         </button>
