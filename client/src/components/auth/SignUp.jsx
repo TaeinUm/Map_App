5bfcae34c748a07c002965c32d43bc2d9ae72ceb
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-03 23:00:42 -0400

    #2 Auth components MUI added

--- client/src/components/auth/SignUp.jsx
+++ client/src/components/auth/SignUp.jsx
@@ -1,56 +1,114 @@
 import React from "react";
-import "../../styles/auth.css";
+import {
+  Box,
+  TextField,
+  Button,
+  Typography,
+  Link,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
 
 function SignUp() {
   return (
-    <div className="sign-container d-flex justify-content-center align-items-center">
-      <div className="sign-image">
-        <img src="https://picsum.photos/id/43/1280/831.jpg" alt="Background" />
-      </div>
-      <div className="sign-form d-flex align-items-center justify-content-center flex-column">
-        <h2>TerraCanvas</h2>
-        <form className="inputform">
-          <div className="d-flex align-items-center justify-content-between">
-            <div className="d-flex flex-column">
-              <div className="d-flex justify-content-between">
-                <label className="input-top">Name</label>
-                <div></div>
-              </div>
-              <input className="nameinput" type="text" placeholder="Name" />
-            </div>
-            <div className="d-flex flex-column">
-              <div className="d-flex justify-content-between">
-                <label className="input-top">Username</label>
-              </div>
-              <input type="text" placeholder="Username" />
-            </div>
-          </div>
-          <div className="d-flex flex-column align-items-center justify-content-center">
-            <div className="d-flex justify-content-between">
-              <label className="input-top">Email</label>
-              <div></div>
-            </div>
-            <input type="email" placeholder="Email" />
-            <div className="d-flex justify-content-between">
-              <label className="input-top">Password</label>
-              <div></div>
-            </div>
-            <input type="password" placeholder="Password" />
-          </div>
-          <div className="agreement d-flex">
-            <input type="checkbox" id="terms" />
-            <label htmlFor="terms">
-              I agree with Dribbble's Terms of Service, Privacy Policy, and
-              default Notification Settings.
-            </label>
-          </div>
-        </form>
-        <button type="submit">Create Account</button>
-        <p>
-          Already have an account? <a href="/signin">Sign In</a>
-        </p>
-      </div>
-    </div>
+    <Box
+      className="sign-container"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flex="1"
+        width="50%"
+        sx={{
+          "@media (max-width: 768px)": {
+            display: "none",
+          },
+        }}
+      >
+        <img
+          src="https://picsum.photos/id/43/1280/831.jpg"
+          alt="Background"
+          style={{
+            objectFit: "cover",
+            height: "100%",
+            width: "100%",
+          }}
+        />
+      </Box>
+      <Box
+        className="sign-form"
+        p={3}
+        display="flex"
+        height="100vh"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        flex="1"
+        bgcolor="white"
+      >
+        <Typography variant="h3" gutterBottom>
+          TerraCanvas
+        </Typography>
+        <Box width="70%">
+          <Box display="flex" justifyContent="space-between" marginBottom={1}>
+            <TextField
+              label="Name"
+              variant="outlined"
+              margin="normal"
+              width="50%"
+              marginRight="10px"
+            />
+            <TextField
+              label="Username"
+              variant="outlined"
+              margin="normal"
+              width="50%"
+            />
+          </Box>
+          <TextField
+            fullWidth
+            label="Email"
+            variant="outlined"
+            margin="normal"
+            sx={{ marginBottom: "20px" }}
+          />
+          <TextField
+            fullWidth
+            label="Password"
+            variant="outlined"
+            margin="normal"
+            type="password"
+            sx={{ marginBottom: "20px" }}
+          />
+          <FormControlLabel
+            sx={{ marginBottom: "20px" }}
+            control={<Checkbox name="agreement" />}
+            label="I agree with Dribbble's Terms of Service, Privacy Policy, and default Notification Settings."
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{
+              borderRadius: "20px",
+              color: "#FAFAFA",
+              backgroundColor: "black",
+              marginBottom: "20px",
+              height: "50px",
+            }}
+          >
+            Create Account
+          </Button>
+        </Box>
+        <Typography variant="body1">
+          Already have an account? <Link href="/signin">Sign In</Link>
+        </Typography>
+      </Box>
+    </Box>
   );
 }
 
