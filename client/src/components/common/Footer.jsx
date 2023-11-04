86ff12d9f60658eec77f2ceb94ad75e41ad0a868
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-03 21:32:17 -0400

    #2 footer design (done)

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,21 +1,46 @@
 import "../../styles/App.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="ftr d-flex justify-content-between">
-      <div type="button" onClick={() => (window.location.href = "/")}>
+    <footer className="ftr d-flex justify-content-between align-items-center">
+      <div
+        className="ftrlogo"
+        type="button"
+        onClick={() => (window.location.href = "/")}
+      >
         TERRACANVAS
       </div>
       <nav>
         <button>
-          <Link to="/termsconditions">Terms and Conditions</Link>
+          <NavLink
+            to="/termsconditions"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+          >
+            Terms and Conditions
+          </NavLink>
         </button>
         <button>
-          <Link to="/privacypolicy">Privacy Policy</Link>
+          <NavLink
+            to="/privacypolicy"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+          >
+            Privacy Policy
+          </NavLink>
         </button>
         <button>
-          <Link to="/contact">Contact</Link>
+          <NavLink
+            to="/contact"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+          >
+            Contact
+          </NavLink>
         </button>
       </nav>
     </footer>
