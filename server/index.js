a812040e75f933d6fbaab5e1051773ff39f57297
YUNBLAK <87653966+YUNBLAK@users.noreply.github.com>
2023-11-03 22:38:53 -0400

    #5 Super Test 1 Passed

--- server/index.js
+++ server/index.js
@@ -9,15 +9,23 @@ app.get('/', (req, res) => {
   res.send('Welcome to TerraCanvas!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
-  app.use((err, req, res, next) => {
-    res.status(500).send('Something broke!');
+app.use((err, req, res, next) => {
+  res.status(500).send('Something broke!');
+});
+
+// Only start listening when the file is run directly, not when imported as a module
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
+}
+
+module.exports = app;
--- server/test/test.js
+++ server/test/test.js
@@ -1,11 +1,22 @@
-// server/test/index.test.js
+// test.js
+require('dotenv').config(); // Ensure environment variables are loaded
 const request = require('supertest');
-const app = require('../index'); // 상대 경로를 사용하여 index.js 파일을 가져옵니다.
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+const app = require('../index'); // Adjust the path to where your Express app is exported
 
 describe('GET /', function() {
   it('responds with Welcome to TerraCanvas!', function(done) {
     request(app)
       .get('/')
-      .expect('Welcome to TerraCanvas!', done);
+      .end(function(err, res) {
+        expect(res.text).to.equal('Welcome to TerraCanvas!');
+        done(err); // Pass the error if there is one to Mocha
+      });
   });
+});
+
+// Optional: Close the mongoose connection after the tests are done
+after(function(done) {
+  mongoose.disconnect(done);
 });
