3c23b23f76105222ee91a9e08c23b75b6faf136d
YUNBLAK <87653966+YUNBLAK@users.noreply.github.com>
2023-11-03 22:19:49 -0400

    #5 rollback

--- server/index.js
+++ server/index.js
@@ -1,10 +1,40 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express();
+
+// // Test Server
+// app.get('/', (req, res) => {
+//   res.send('Welcome to TerraCanvas!');
+// });
+
+// // Connect to MongoDB
+// mongoose.connect(process.env.MONGODB_URI)
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error('Could not connect to MongoDB', err));
+
+// app.use((err, req, res, next) => {
+//   res.status(500).send('Something broke!');
+// });
+
+// // 서버 시작 부분을 주석 처리합니다.
+// // app.listen(PORT, () => {
+// //   console.log(`Server is running on port ${PORT}`);
+// // });
+
+// module.exports = app; // app 객체를 내보냅니다.
+
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Test Server
 app.get('/', (req, res) => {
   res.send('Welcome to TerraCanvas!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Connect to MongoDB
@@ -12,13 +42,6 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
-app.use((err, req, res, next) => {
-  res.status(500).send('Something broke!');
-});
-
-// 서버 시작 부분을 주석 처리합니다.
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-module.exports = app; // app 객체를 내보냅니다.
+  app.use((err, req, res, next) => {
+    res.status(500).send('Something broke!');
+  });
