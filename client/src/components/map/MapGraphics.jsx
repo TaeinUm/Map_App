ddf2159c21196d894e5b2b7b5870d2efe34c1774
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-04 01:07:50 -0400

    #3 select map graphics type modal design (done)

--- client/src/components/map/MapGraphics.jsx
+++ client/src/components/map/MapGraphics.jsx
@@ -1,5 +1,14 @@
-import React from "react";
-import { Fade, Box, Typography, Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Fade,
+  Typography,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Button,
+} from "@mui/material";
 
 const style = {
   position: "absolute",
@@ -14,17 +23,94 @@ const style = {
 };
 
 function MapGraphics(open) {
+  const [selectedType, setSelectedType] = useState(null);
+
+  const handleSelect = (type) => {
+    setSelectedType(type);
+  };
+
+  const isSelected = (type) => {
+    return selectedType === type;
+  };
+
   return (
     <div>
       <Fade in={open}>
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
+        <Grid
+          container
+          spacing={2}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "70%",
+            height: "auto",
+            padding: "20px",
+            backgroundColor: "white",
+            borderRadius: "10px",
+          }}
+        >
+          <Grid item xs={12}>
+            <Typography
+              variant="h5"
+              style={{ textAlign: "center", fontWeight: "bold" }}
+            >
+              Select Your Map Graphics Type
+            </Typography>
+          </Grid>
+          {[
+            "Heat Map",
+            "Point Map",
+            "Regional Map",
+            "Flow Map",
+            "3D Bar Map",
+            "Basic Map",
+          ].map((type, index) => (
+            <Grid item xs={6} sm={4} key={type}>
+              <Card>
+                <CardActionArea onClick={() => handleSelect(type)}>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={`/path-to-images/${type
+                      .replace(/\s+/g, "-")
+                      .toLowerCase()}.jpg`}
+                    alt={type}
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+                      component="div"
+                      style={{
+                        textAlign: "center",
+                        textDecoration: isSelected(type) ? "underline" : "none", // Apply underline conditionally
+                      }}
+                    >
+                      {type}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+          <Grid item xs={12} style={{ textAlign: "center", marginTop: "20px" }}>
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{
+                borderRadius: "5px",
+                color: "#FAFAFA",
+                backgroundColor: "black",
+                height: "50px",
+                width: "200px",
+              }}
+            >
+              Start New
+            </Button>
+          </Grid>
+        </Grid>
       </Fade>
     </div>
   );
