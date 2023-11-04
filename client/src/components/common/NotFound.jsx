65b7e8e0da614c3a3a3f99afa07e14ec9b9fd757
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-03 21:38:33 -0400

    #1 page not found

--- client/src/components/common/NotFound.jsx
+++ client/src/components/common/NotFound.jsx
@@ -21,7 +21,7 @@ const NotFound = () => {
   return (
     <div style={styles.container}>
       <h1 style={styles.header}>404</h1>
-      <p style={styles.message}>페이지를 찾을 수 없습니다.</p>
+      <p style={styles.message}>Page Not Found.</p>
     </div>
   );
 };
