ac50f19df0603fd56e8cfee0242c6ce64badc348
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-05 04:19:32 -0500

    #1 axios used

--- client/src/api/graphicsAPI.js
+++ client/src/api/graphicsAPI.js
@@ -1,20 +1,13 @@
+import axios from "axios";
+
 const BASE_URL = "BACKEND_API_URL";
 
 export const getTop5Trending = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/api/top5graphics`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      return await response.json();
-    } else {
-      throw new Error("Failed to fetch trending graphics");
-    }
+    const response = await axios.get(`${BASE_URL}/api/top5graphics`);
+    return response.data;
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching top graphics:", error);
     throw error;
   }
 };
