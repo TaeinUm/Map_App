9098515f8c9bf927e03f923639739cf3446006e9
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-05 23:36:43 -0500

    #2 Simple cookie for login status

--- client/src/api/authService.js
+++ client/src/api/authService.js
@@ -2,31 +2,37 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
+const authAPI = async (method, url, data) => {
+  try {
+    const response = await axios[method](url, data);
+    return { success: true, data: response.data };
+  } catch (error) {
+    console.error(`Error during ${method} request to ${url}:`, error);
+    return {
+      success: false,
+      message: error?.response?.data?.error || "An error occurred.",
+    };
+  }
+};
+
 const getLoggedIn = async () => {
-  const response = await axios.get(`${BASE_URL}/auth/loggedIn`);
-  return response.data;
+  return await authAPI("get", `${BASE_URL}/auth/loggedIn`);
 };
 
 const login = async (email, password) => {
-  const response = await axios.post(`${BASE_URL}/auth/login`, {
-    email,
-    password,
-  });
-  return response.data;
+  return await authAPI("post", `${BASE_URL}/auth/login`, { email, password });
 };
 
 const logout = async () => {
-  const response = await axios.post(`${BASE_URL}/auth/logout`);
-  return response.data;
+  return await authAPI("post", `${BASE_URL}/auth/logout`);
 };
 
 const register = async (userName, email, password) => {
-  const response = await axios.post(`${BASE_URL}/auth/register`, {
+  return await authAPI("post", `${BASE_URL}/auth/register`, {
     userName,
     email,
     password,
   });
-  return response.data;
 };
 
 export { getLoggedIn, login, logout, register };
--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,35 +1,51 @@
 import React, { createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { getLoggedIn, login } from "../api/authService";
+import { getLoggedIn, login, logout } from "../api/authService";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    JSON.parse(localStorage.getItem("isAuthenticated")) || false
+  );
   const navigate = useNavigate();
 
+  /** We need to change this into backend HTTP-only cookie for security!  */
   useEffect(() => {
     const checkLoggedIn = async () => {
       try {
-        const isLoggedIn = await getLoggedIn();
-        setIsAuthenticated(isLoggedIn);
+        const { success, data } = await getLoggedIn();
+        setIsAuthenticated(success && data.isAuthenticated);
+        localStorage.setItem(
+          "isAuthenticated",
+          JSON.stringify(success && data.isAuthenticated)
+        );
       } catch (error) {
-        console.error("Failed to verify login status:", error);
+        console.error("Failed to get loggin status:", error);
+        setIsAuthenticated(false);
+        localStorage.setItem("isAuthenticated", "false");
       }
     };
 
-    checkLoggedIn();
+    if (!isAuthenticated) {
+      checkLoggedIn();
+    }
   }, []);
 
   const handleLogin = async (email, password) => {
     try {
-      const data = await login(email, password);
-      if (data) {
+      const { success, data } = await login(email, password);
+      if (success && data) {
         setIsAuthenticated(true);
+        localStorage.setItem("isAuthenticated", "true");
         navigate("/");
+      } else {
+        console.error("Login Failed.");
+        alert("Failed to log in.");
       }
     } catch (error) {
       console.error("Login failed:", error);
+      alert("Failed to loginn due to an error.");
     }
   };
 
@@ -39,9 +55,21 @@ export const AuthProvider = ({ children }) => {
     navigate("/");
   };*/
 
-  const handleLogout = () => {
-    setIsAuthenticated(false);
-    navigate("/");
+  const handleLogout = async () => {
+    try {
+      const { success } = await logout();
+      if (success) {
+        setIsAuthenticated(false);
+        localStorage.setItem("isAuthenticated", "false");
+        navigate("/");
+      } else {
+        console.error("Logout failed.");
+        alert("Failed to logout.");
+      }
+    } catch (error) {
+      console.error("Logout failed:", error);
+      alert("Failed to logout due to an error.");
+    }
   };
 
   return (
