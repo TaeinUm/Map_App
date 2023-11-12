9a249b1053995d6476ef89b89a380d2c288c8873
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-11 19:11:48 -0500

    #3 add map graphics styles controlers

--- client/src/components/map/tab/StylesTab.jsx
+++ client/src/components/map/tab/StylesTab.jsx
@@ -1,26 +1,30 @@
 import React, { useContext } from "react";
 import Basic from "./control/Basic";
 import Point from "./control/Point";
+import Heat from "./control/Heat";
+import Regional from "./control/Regional";
+import Flow from "./control/Flow";
+import ThreeD from "./control/ThreeD";
 import { MapContext } from "../../../contexts/MapContext";
 import { Link } from "react-router-dom";
 
-function StylesTab() {
+function StylesTab({ onChange, setGeojsonData }) {
   const { mapType } = useContext(MapContext);
 
   let content;
   if (mapType === "Basic Map") {
-    content = <Basic />;
+    content = <Basic onChange={onChange} setGeojsonData={setGeojsonData} />;
   } else if (mapType === "Point Map") {
-    content = <Point />;
-  } /*else if (mapType === "Heat Map") {
-    content = <Heat />;
+    content = <Point onChange={onChange} setGeojsonData={setGeojsonData} />;
+  } else if (mapType === "Heat Map") {
+    content = <Heat onChange={onChange} setGeojsonData={setGeojsonData} />;
   } else if (mapType === "Regional Map") {
-    content = <Regional />;
+    content = <Regional onChange={onChange} setGeojsonData={setGeojsonData} />;
   } else if (mapType === "Flow Map") {
-    content = <Flow />;
+    content = <Flow onChange={onChange} setGeojsonData={setGeojsonData} />;
   } else if (mapType === "3D-Bar Map") {
-    content = <ThreeD />;
-  } */ else {
+    content = <ThreeD onChange={onChange} setGeojsonData={setGeojsonData} />;
+  } else {
     content = <Link to="*" />;
   }
 
--- client/src/components/map/tab/control/Point.jsx
+++ client/src/components/map/tab/control/Point.jsx
@@ -1,60 +1,124 @@
-import React, { useState, useContext } from "react";
-import * as XLSX from "xlsx";
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
-import { MapContext } from "../../../../contexts/MapContext";
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { fromJS } from "immutable";
+import MAP_STYLE from "../../map-style-basic-v8.json";
+import { Typography, Divider, Box } from "@mui/material";
 
-const Point = () => {
-  const { setGeojsonData } = useContext(MapContext);
-  const [file, setFile] = useState(null);
+const defaultMapStyle = fromJS(MAP_STYLE);
+const defaultLayers = defaultMapStyle.get("layers");
 
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+const categories = [
+  "labels",
+  "roads",
+  "buildings",
+  "parks",
+  "water",
+  "background",
+];
+
+// Layer id patterns by category
+const layerSelector = {
+  background: /background/,
+  water: /water/,
+  parks: /park/,
+  buildings: /building/,
+  roads: /bridge|road|tunnel/,
+  labels: /label|place|poi/,
+};
+
+// Layer color class by type
+const colorClass = {
+  line: "line-color",
+  fill: "fill-color",
+  background: "background-color",
+  symbol: "text-color",
+};
+
+function getMapStyle({ visibility, color }) {
+  const layers = defaultLayers
+    .filter((layer) => {
+      const id = layer.get("id");
+      return categories.every(
+        (name) => visibility[name] || !layerSelector[name].test(id)
+      );
+    })
+    .map((layer) => {
+      const id = layer.get("id");
+      const type = layer.get("type");
+      const category = categories.find((name) => layerSelector[name].test(id));
+      if (category && colorClass[type]) {
+        return layer.setIn(["paint", colorClass[type]], color[category]);
+      }
+      return layer;
+    });
+
+  return defaultMapStyle.set("layers", layers);
+}
+
+function StyleControls(props) {
+  const [visibility, setVisibility] = useState({
+    water: true,
+    parks: true,
+    buildings: true,
+    roads: true,
+    labels: true,
+    background: true,
+  });
+
+  const [color, setColor] = useState({
+    water: "#DBE2E6",
+    parks: "#E6EAE9",
+    buildings: "#c0c0c8",
+    roads: "#ffffff",
+    labels: "#78888a",
+    background: "#EBF0F0",
+  });
+
+  useEffect(() => {
+    props.setGeojsonData(fromJS(MAP_STYLE));
+    props.onChange(getMapStyle({ visibility, color }));
+  }, [visibility, color]);
+
+  const onColorChange = (name, value) => {
+    setColor({ ...color, [name]: value });
   };
 
-  const handleFileUpload = async () => {
-    if (!file) return;
-
-    const data = await file.arrayBuffer();
-    const workbook = XLSX.read(data);
-    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-    const jsonData = XLSX.utils.sheet_to_json(worksheet);
-
-    const newGeojsonData = {
-      type: "FeatureCollection",
-      features: [],
-    };
-
-    for (const address of jsonData) {
-      const query = address["Address Column Name"]; // replace 'Address Column Name' with the actual column name from your Excel file
-      const geocoder = new MapboxGeocoder({
-        accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN, // make sure to use your Mapbox access token
-        query,
-      });
-
-      geocoder.on("result", (e) => {
-        const { result } = e;
-        const location = result.geometry.coordinates;
-        const feature = {
-          type: "Feature",
-          properties: {},
-          geometry: {
-            type: "Point",
-            coordinates: location,
-          },
-        };
-        newGeojsonData.features.push(feature);
-      });
-    }
-
-    setGeojsonData(newGeojsonData); // Update the geojson data in context
+  const onVisibilityChange = (name, value) => {
+    setVisibility({ ...visibility, [name]: value });
   };
 
   return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleFileUpload}>Upload and Generate Map</button>
-    </div>
+    <Box className="control-panel">
+      <Typography variant="h5" sx={{ textAlign: "left", color: "#fafafa" }}>
+        Color
+      </Typography>
+      <Divider sx={{ borderColor: "#fafafa" }} />
+      <hr />
+      {categories.map((name) => (
+        <Box
+          key={name}
+          className="input"
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Typography sx={{ color: "#fafafa" }}>{name}</Typography>
+          <Typography sx={{ color: "#fafafa" }}>{color[name]}</Typography>
+          <Box>
+            <input
+              type="checkbox"
+              checked={visibility[name]}
+              onChange={(evt) => onVisibilityChange(name, evt.target.checked)}
+            />
+            <input
+              type="color"
+              value={color[name]}
+              disabled={!visibility[name]}
+              onChange={(evt) => onColorChange(name, evt.target.value)}
+            />
+          </Box>
+        </Box>
+      ))}
+    </Box>
   );
-};
+}
 
-export default Point;
+export default React.memo(StyleControls);
