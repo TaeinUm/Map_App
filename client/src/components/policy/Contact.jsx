17627223b4412d03767948df1c7da320ba60318a
BallisticDemon <albert.guan@stonybrook.edu>
2023-11-13 11:00:00 -0500

    All the pages that the footer links to plus policy css

--- client/src/components/policy/Contact.jsx
+++ client/src/components/policy/Contact.jsx
@@ -2,6 +2,39 @@ import React from "react";
 
 
 function Contact() {
-  return <div>contact</div>;
+  return <div className="terms-conditions">
+  <h1><b>Contact TerraCanvas</b></h1>
+  <p className="update-date">Last updated: 2023.10.02</p>
+
+  <br></br>
+  <br></br>
+
+  <section>
+    <h2>General Inquiries</h2>
+    <p>
+      Welcome to <strong>TerraCanvas</strong>, your prime destination for map graphics and community services. 
+      Our platform also encompasses a variety of related products and services, all tailored to meet your mapping needs.
+    </p>
+    <p>
+      For any questions or assistance, feel free to reach out to us. We are here to help!
+    </p>
+  </section>
+  <br></br>
+  <br></br>
+  <h2>Contact Details</h2>
+  <strong>President: </strong>Taein Um
+  <br></br>
+  <strong>Email: </strong> <a href="mailto:taein.um@stonybrook.edu">taein.um@stonybrook.edu</a>
+  <br></br>
+  <strong>Address: </strong> 60 North Country Road, East Setauket, New York, United States
+  <br></br>
+  <strong>Phone: </strong> 631-352-6530
+  <br></br>
+  <br></br>
+  <br></br>
+  <br></br>
+  <br></br>
+ 
+</div>;
 }
 export default Contact;
--- client/src/components/policy/TermsConditions.jsx
+++ client/src/components/policy/TermsConditions.jsx
@@ -2,6 +2,43 @@ import React from "react";
 
 
 function Termsconditions() {
-  return <div>terms!!</div>;
+
+  return (
+    <div className="terms-conditions">
+      <h1><b>Terms and Conditions (EXAMPLE)</b></h1>
+      <p>Last updated: 2023.11.12</p>
+      <p>Please red these terms and conditions of use carefully before accessing, 
+        using or obtaining any materials, information, products or services.</p>
+      <br></br>
+      
+      <h2>Agreement to our Legal Terms</h2>
+      <br></br>
+      <p>We are ‘TerraCanvas.’</p>
+      <p>We operate the website to provide map graphics and community services, 
+        as well as any other related products and services that refer or link to these legal terms.</p>
+      <p>You can contact us by email at _________ or by mail to _________.</p>
+      <br></br>
+      <p>These Legal Terms constitute a legally binding agreement made between you, 
+        whether personally or on behalf of an entity ("you"), and TerraCanvas, concerning your access to 
+        and Use of the Services. You agree that by accessing the Services, you have read, understood, 
+        and agreed to be bound by all of these Legal Terms. 
+        IF YOU DO NOT AGREE WITH ALL OF THESE LEGAL TERMS, 
+        THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SERVICES 
+        AND YOU MUST DISCOUNTINUE USE IMMEDIATELY.</p>
+      <br></br>
+      <p>Supplemental terms and conditions or documents that may be posted on the Services 
+        from time to time are hereby expressly incorporated herein by reference.</p>
+      <p>We reserve the right, in our sole discretion, to make changes or modification to these Legal Terms 
+        at any time and for any reason. We will alert you about any changes by updating 
+        The "Last updated" date of these Legal Terms, and you waive any right to receive specific notice of 
+        each such change. It is your responsibility to periodically review these Legal Terms 
+        to stay informed of updates. You will be subject to, and will be deemed to have been made aware of 
+        and to have accepted, the changes in any revised Legal Terms by your Continued use of the Services 
+        after the date such revised Legal Terms are posted.</p>
+      <br></br>
+      <p>We recommend that you print a copy of these Legal Terms for your records.</p>
+    </div>
+  );
+
 }
 export default Termsconditions;
