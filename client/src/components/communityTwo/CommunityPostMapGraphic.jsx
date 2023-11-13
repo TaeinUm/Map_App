e1077e1f7fd5c133eca4546ae1b2133f114d5024
YUNBLAK <87653966+YUNBLAK@users.noreply.github.com>
2023-11-13 05:53:09 -0500

    #3 Posting Page

--- client/src/components/communityTwo/CommunityPostMapGraphic.jsx
+++ client/src/components/communityTwo/CommunityPostMapGraphic.jsx
@@ -1,25 +1,71 @@
-import React, { useState, useEffect } from "react";
-import {Box, Typography, TextField, Button, Paper, Input} from "@mui/material";
-import FileLoader from "./FileLoader";
+import React, { useState } from 'react';
+import { Box, Typography, Radio, RadioGroup, FormControlLabel, FormControl, TextField, Button, Paper } from '@mui/material';
+import FileLoader from './FileLoader';
+
+function CommunityPostMapGraphic() {
+  const [postType, setPostType] = useState('Map Graphics');
+
+  return (
+    <Paper style={{ padding: '2rem', backgroundColor: '#212121', color: 'white', margin: '2rem' }}>
+      <Typography variant="h5" gutterBottom>Type</Typography>
+      <FormControl component="fieldset">
+        <RadioGroup
+          row
+          aria-label="post-type"
+          name="postType"
+          value={postType}
+          onChange={(event) => setPostType(event.target.value)}
+        >
+          <FormControlLabel value="Map Graphics" control={<Radio />} label="Map Graphics" />
+          <FormControlLabel value="Map Ideas" control={<Radio />} label="Map Ideas" />
+          <FormControlLabel value="Questions" control={<Radio />} label="Questions" />
+        </RadioGroup>
+      </FormControl>
+
+      <Typography variant="h5" gutterBottom style={{ marginTop: '1rem' }}>Title</Typography>
+      <TextField 
+        fullWidth 
+        variant="outlined" 
+        placeholder="Enter the title of your post here" 
+        style={{ backgroundColor: 'white' }}
+      />
+
+      <Typography variant="h5" gutterBottom style={{ marginTop: '1rem' }}>Content</Typography>
+      <TextField 
+        fullWidth 
+        multiline 
+        rows={10} 
+        variant="outlined" 
+        placeholder="Enter the content of your post here" 
+        style={{ backgroundColor: 'white' }}
+      />
 
 
-function CommunityPostMapGraphic(){
-    return(
-        <div>
-            <h3 style={{color:'white'}}>Type</h3>
-            <input type="radio" id="Map Graphics" value="HTML"></input>
-            <label style={{color:'white'}} for="Map Graphics">Map Graphics</label>
-            <input type="radio" id="Map Ideas" value="HTML"></input>
-            <label style={{color:'white'}} for="Map Ideas">Map Ideas</label>
-            <input type="radio" id="question" value="Bike"></input>
-            <label style={{color:'white'}} for="question">Question</label><br></br>
-            <h3 style={{color:'white'}}>Title</h3>
-            <textarea style={{width:"75%"}} rows={1} placeholder="Enter the title of your post here"></textarea>
-            <h3 style={{color:'white'}}>Content</h3>
-            <textarea style={{width:"75%", height:"400px"}} rows={5} placeholder="Enter the content of your post here"></textarea>
+      <Box display="flex" justifyContent="space-between" marginTop="1rem">
+        <Button variant="contained" color="primary" component="label">
+          Load Local Files
+          <input type="file" hidden />
+        </Button>
+        <Button variant="contained" color="primary" component="label">
+          Load From Storage
+          {/* Implement storage loading functionality */}
+        </Button>
+      </Box>
 
-            <FileLoader></FileLoader>
-        </div>
-    );
+      <Box display="flex" justifyContent="center" marginTop="1rem">
+        <Button variant="contained" color="secondary" size="large">
+          Post
+        </Button>
+      </Box>
+
+      <Box textAlign="center" marginTop="4rem" paddingBottom="1rem">
+        <Typography variant="caption">
+          TERRACANVAS - Terms and Conditions Privacy Policy Contact
+        </Typography>
+      </Box>
+    </Paper>
+  );
 }
+
 export default CommunityPostMapGraphic;
+
