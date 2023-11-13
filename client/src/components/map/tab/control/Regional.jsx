e8c1c4694c75c452c79bc18cf44a07f48baf74e9
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-12 22:33:17 -0500

    #3 regional map done

--- client/src/components/map/tab/control/Regional.jsx
+++ client/src/components/map/tab/control/Regional.jsx
@@ -251,59 +251,39 @@ const Regional = () => {
   };
 
   useEffect(() => {
-    const initializeMap = () => {
-      const newMap = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: mapStyle,
-        center: [-74.006, 40.7128],
-        zoom: 2,
+    const newMap = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-74.006, 40.7128],
+      zoom: 2,
+    });
+
+    newMap.on("load", () => {
+      newMap.addSource("countries", {
+        type: "vector",
+        url: "mapbox://mapbox.country-boundaries-v1",
       });
 
-      newMap.on("load", () => {
-        newMap.addLayer({
-          id: "country-boundaries",
-          type: "fill",
-          source: {
-            type: "vector",
-            url: "mapbox://mapbox.country-boundaries-v1",
-          },
-          "source-layer": "country_boundaries",
-          paint: {
-            "fill-opacity": 0,
-          },
-        });
-
-        newMap.on("click", "country-boundaries", function (e) {
-          const countryCode = e.features[0].properties.iso_3166_1_alpha_3;
-          const countryName = e.features[0].properties.name;
-
-          newMap.setFilter("country-boundaries", [
-            "==",
-            ["get", "iso_3166_1_alpha_3"],
-            countryCode,
-          ]);
-
-          newMap.setPaintProperty(
-            "country-boundaries",
-            "fill-color",
-            regionColor
-          );
-
-          setSelectedCountry(countryName);
-        });
-
-        setMap(newMap);
-        newMap.resize();
+      newMap.addLayer({
+        id: "countries",
+        type: "fill",
+        source: "countries",
+        "source-layer": "country_boundaries",
+        paint: {
+          "fill-color": "#FFFFFF",
+          "fill-opacity": 0.4,
+        },
       });
-    };
-    if (!map) {
-      initializeMap();
-    }
+
+      setMap(newMap);
+    });
 
     if (map) {
       setMapJson(map.getStyle());
     }
-  }, [map, mapStyle, regionColor, setMap, setSelectedCountry]);
+
+    return () => newMap.remove();
+  }, []);
 
   useEffect(() => {
     if (map && log.length > 0) {
@@ -356,9 +336,6 @@ const Regional = () => {
         }
 
         colorExpression.push("#FFFFFF");
-
-        console.log(colorExpression);
-
         map.setPaintProperty("countries", "fill-color", colorExpression);
       }
     }
@@ -369,7 +346,7 @@ const Regional = () => {
     updatedLog.push({ country: selectedCountry, color: color });
 
     setLog(updatedLog);
-    updateMapColors(updatedLog);
+    updateMapColors();
   };
 
   const handleContinentSelect = (continent) => {
