3c1e726e7ae86dc78537c7ebe7cdfcbb027d40e1
BallisticDemon <albert.guan@stonybrook.edu>
2023-11-16 18:38:35 -0500

    Community api endpoints make post

---
+++ client/src/api/CommunitySectionAPI.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { AuthContext } from "../contexts/AuthContext";
+import React, { useContext } from "react";
+
+const API_BASE_URL =
+  "https://terracanvas-fb4c23ffbf5d.herokuapp.com" || "http://localhost:8080";
+
+  const CommunitySectionAPI = {
+    //make a post
+    makePost: async (userId, postType, content, date) => {
+        const { isAuthenticated, userId, username } = useContext(AuthContext);
+
+  if (!isAuthenticated) {
+    console.error("User is not authenticated");
+    return;
+  }
+        try {
+        //   const response = await axios.get(
+        //     `${API_BASE_URL}/api/community/post`
+        //   );
+          return await CommunitySectionAPI("post", `${API_BASE_URL}/api/community/post/${userId}`,{
+
+            postType, 
+            postText,
+            postFile,
+            date,
+
+          }  );
+        } catch (error) {
+          console.error("cannot make a post.");
+        }
+      },
+  }
