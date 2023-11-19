6b6819aafc4a3cdc6113bfd8751713dbca2044db
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-19 03:34:55 -0500

    post.js added

---
+++ server/models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
+  postId: Number,
+  date: Date,
+  content: String,
+  likes: Number,
+  types: String,
+  image: String,
+  title: String
+});
+
+const Post = mongoose.model('Post', postSchema, 'posts');
+
+module.exports = Post;
