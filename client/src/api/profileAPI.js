e16c213eb9cf6c4ee5670fbeb156f3f98701d046
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-18 22:15:05 -0500

    #2 profile API added

--- client/src/api/profileAPI.js
+++ client/src/api/profileAPI.js
@@ -10,26 +10,60 @@ import { AuthContext } from "../contexts/AuthContext";
 const BASE_URL =
   "https://terracanvas-fb4c23ffbf5d.herokuapp.com" || "http://localhost:8080";
 
-//get postings of the certain users
-//check the login status first
-export const getPostings = async () => {
-  const { isAuthenticated, userId, username } = useContext(AuthContext);
+const userAPI = {
+  //upate profile image
+  updateProfilePicture: async (userId, username, imageData) => {
+    const formData = new FormData();
+    formData.append("image", imageData);
 
-  if (!isAuthenticated) {
-    console.error("User is not authenticated");
-    return;
-  }
+    try {
+      const response = await axios.put(
+        `${BASE_URL}/api/users/${userId}/profile-picture`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          params: { username },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error updating profile picture:", error);
+      throw error;
+    }
+  },
 
-  try {
-    const response = await axios.get(`${BASE_URL}/api/postings`, {
-      params: {
-        userId,
-        username,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching postings:", error);
-    throw error;
-  }
+  //update user details info, if not changed, x change those info
+  updateUserDetails: async (userId, newEmail, newUsername, newPassword) => {
+    try {
+      const response = await axios.put(`${BASE_URL}/api/users/${userId}`, {
+        email: newEmail,
+        username: newUsername,
+        password: newPassword,
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error updating user details:", error);
+      throw error;
+    }
+  },
+
+  //get all postings by userId, uesrname
+  getPostings: async (userId, username) => {
+    try {
+      const response = await axios.get(`${BASE_URL}/api/postings`, {
+        params: {
+          userId,
+          username,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching postings:", error);
+      throw error;
+    }
+  },
 };
+
+export default userAPI;
