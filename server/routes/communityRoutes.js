abe7dc0f0eea6eeed1bf8c4c3c6a59ea704a718a
YUNBLAK <87653966+YUNBLAK@users.noreply.github.com>
2023-11-22 08:28:50 -0500

    #5 API mod

--- server/routes/communityRoutes.js
+++ server/routes/communityRoutes.js
@@ -5,30 +5,37 @@ const searchController = require('../controllers/searchController');
 
 const router = express.Router();
 
+//
 router.post('/post', postController.writePost);
 
+//
 router.get('/getAllPosts', postController.getAllPosts);
 
+//
 router.get('/getMapsByUsername/:userId', searchController.searchMapByUserName);
 
+//
 router.post('/postComment', commentController.writeComment);
 
-
-
+//
 router.get('/getallComments', commentController.getAllComments);
 
+//
 router.get('/getAllCommentsByPostID/:postId', commentController.getAllCommentsByPostID);
 
-
-
+//
 router.get('/getQuestions/:searchText', searchController.searchQuestionByText);
 
+//
 router.get('/getIdeas/:searchText', searchController.searchIdeaByText);
 
+//
 router.get('/getMapsBySearch/:searchText', searchController.searchMapByText);
 
+//
 router.put('/likeMap/:postId', postController.likePost)
 
+//
 router.put('/unlikeMap/:postId', postController.unlikePost)
 
 module.exports = router;
