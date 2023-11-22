f6b6e2c2935012330c2a672e6cce8c7b7f3bad03
Taein Um <taein.um@stonybrook.edu>
2023-11-22 18:14:41 -0500

    #5 Unused routes deleted

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -8,7 +8,7 @@ const upload = multer({ dest: 'uploads/' }); // Adjust the destination as needed
 
 
 // Update profile picture
-router.put('/:userId/profile-picture', upload.single('image'), userController.updateProfilePicture);
+
 
 // Update user details
 router.put('/:userId', userController.updateUserDetails);
@@ -17,6 +17,6 @@ router.put('/:userId', userController.updateUserDetails);
 
 
 // Route to get email by userId
-router.get('/api/users/:userId/email', userController.getEmail);
+
 
 module.exports = router;
