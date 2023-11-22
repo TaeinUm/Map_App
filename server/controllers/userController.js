76593a2c4f381b226266c4fc7d4848f4f93dba25
Taein Um <taein.um@stonybrook.edu>
2023-11-22 18:06:33 -0500

    #5 Routes modified

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,108 +1,35 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User'); // Adjust the path according to your structure
-const session = require('express-session');
 
-// Register a new user
-const registerUser = async (req, res) => {
-  try {
-    // Hash password
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-    // Create a new user instance
-    const user = new User({
-      userName: req.body.userName,
-      email: req.body.email,
-      password: hashedPassword,
-      profileImage: ""
-    });
-
-    // Save the user to the database
-    const savedUser = await user.save();
-    res.status(201).json(savedUser);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// Login a user
-const loginUser = async (req, res) => {
-  try {
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(401).json({ message: "Email does not exist" });
-    }
-    const isMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ message: "Password is incorrect" });
-    }
-    // User is authenticated, create session
-    req.session.userId = user._id;
-
-    // Send the user ID in the response
-    res.json({ message: "Logged in successfully", userId: user._id });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// Logout a user
-const logoutUser = (req, res) => {
-  req.session.destroy(err => {
-    if (err) {
-      res.status(400).json({ message: "Unable to log out" });
-    } else {
-      res.json({ message: "Logout successful" });
-    }
-  });
-};
-
-// Check if a user is logged in and return user data
-const checkLoggedIn = async (req, res) => {
-  const { email } = req.params;
-
-  try {
-    const user = await User.findOne({ email }).select('-password');
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    res.json({ loggedIn: true, user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
 
 // Update user details
 const updateUserDetails = async (req, res) => {
-  try {
-    const { email } = req.params;
-    const { newEmail, newUsername, newPassword } = req.body;
+  const { userId } = req.params;
+  const { email, userName, password } = req.body;
 
-    // Find user by email
-    let user = await User.findOne({ email });
+  try {
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Update user details
-    user.email = newEmail || user.email;
-    user.userName = newUsername || user.userName;
-    if (newPassword) {
-      user.password = await bcrypt.hash(newPassword, 10);
+    // Update fields if they are provided
+    if (email) user.email = email;
+    if (userName) user.userName = userName;
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.password = hashedPassword;
     }
 
     // Save the updated user
-    await user.save();
-
-    res.json({ message: 'User details updated successfully' });
+    const updatedUser = await user.save();
+    res.status(200).json({ message: "User updated successfully", user: updatedUser });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: "Error updating user: " + error.message });
   }
 };
 
+
 module.exports = {
-  registerUser,
-  loginUser,
-  logoutUser,
-  checkLoggedIn,
   updateUserDetails,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const multer = require('multer');
 const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-// Update user details
-router.put('/update/:email', userController.updateUserDetails);
+const upload = multer({ dest: 'uploads/' }); // Adjust the destination as needed
 
-// LoggedIn route
-router.get('/loggedIn/:email', userController.checkLoggedIn);
+
+// Update profile picture
+router.put('/:userId/profile-picture', upload.single('image'), userController.updateProfilePicture);
+
+// Update user details
+router.put('/:userId', userController.updateUserDetails);
+
+// Route to get posts
+
+
+// Route to get email by userId
+router.get('/api/users/:userId/email', userController.getEmail);
 
 module.exports = router;
