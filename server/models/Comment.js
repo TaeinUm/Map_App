85e46f15d5e71a338f45b3f6efb7ff766209310b
YUNBLAK <87653966+YUNBLAK@users.noreply.github.com>
2023-11-24 20:04:12 -0500

    #5 Data Model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -4,8 +4,8 @@ const commentSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   userId: mongoose.Schema.Types.ObjectId,
   postId: mongoose.Schema.Types.ObjectId,
-  date: Date,
-  content: String
+  commentDate: Date,
+  commentContent: String
 });
 
 const Comment = mongoose.model('Comment', commentSchema, 'comments');
