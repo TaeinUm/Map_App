a1872a1db2d3af9228c81af22d74b2ba3cd20eab
Taein Um <taein.um@stonybrook.edu>
2023-11-24 22:28:34 -0500

    #5 Error handling

--- server/server.js
+++ server/server.js
@@ -35,15 +35,18 @@ app.use(session({
 
 // Route handlers
 app.use('/auth', authRoutes); // Authentication routes
+app.use('/api', postRoutes); // Post-related routes
 app.use('/api/users', userRoutes); // User-related routes
 app.use('/api/community', communityRoutes); // Community-related routes
-app.use('/api', postRoutes); // Post-related routes
 app.get('/api/test-data', testController.getDataFromTestCollection); // Test data route
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB:', err));
+  .catch(err => {
+    console.error('Could not connect to MongoDB:', err);
+    process.exit(1); // Optionally exit the process if the database connection fails
+  });
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, '../client/build')));
@@ -56,7 +59,18 @@ app.get('/', (req, res) => {
 // Global error handler
 app.use((err, req, res, next) => {
   console.error("Error:", err.message);
-  res.status(500).send('Something broke!');
+
+  // Set the status code based on the error type
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+
+  // Send a detailed error message in development, simpler message in production
+  const response = {
+    message: process.env.NODE_ENV === 'development' ? err.message : 'An error occurred',
+    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+  };
+
+  res.json(response);
 });
 
 // Start the server
