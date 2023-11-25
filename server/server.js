e4bd29178bd3539ed8b278cf357093550a9fe7a9
Taein Um <taein.um@stonybrook.edu>
2023-11-25 01:41:45 -0500

    #5 Refresh error fixed

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// Load environment variables
+// Load environment variables from .env file
 require('dotenv').config();
 
 // Import required modules
@@ -9,49 +9,54 @@ const cors = require('cors');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
-// Import routes
+// Import route handlers
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const communityRoutes = require('./routes/communityRoutes');
 const postRoutes = require('./routes/postRoutes');
 const testController = require('./controllers/testController');
 
-// Initialize Express app
+// Initialize Express application
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Middleware setup
-app.use(cors()); // Enable CORS
-app.use(express.json()); // Parse JSON bodies
+// Apply middleware
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+app.use(express.json()); // Parse incoming JSON payloads
 
-// Session configuration
+// Configure session management
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
-  cookie: { secure: process.env.NODE_ENV === 'production' },
+  secret: process.env.SESSION_SECRET, // Secret key for signing the session ID cookie
+  resave: false, // Avoid resaving sessions that haven't been modified
+  saveUninitialized: false, // Don't create session until something is stored
+  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }), // Use MongoDB for session storage
+  cookie: { secure: process.env.NODE_ENV === 'production' }, // Use secure cookies in production
 }));
 
-// Route handlers
-app.use('/auth', authRoutes); // Authentication routes
-app.use('/api', postRoutes); // Post-related routes
-app.use('/api/users', userRoutes); // User-related routes
-app.use('/api/community', communityRoutes); // Community-related routes
-app.get('/api/test-data', testController.getDataFromTestCollection); // Test data route
+// Define routes
+app.use('/auth', authRoutes); // Routes for authentication
+app.use('/api/users', userRoutes); // Routes for user-related operations
+app.use('/api/community', communityRoutes); // Routes for community-related operations
+app.use('/api', postRoutes); // Routes for post-related operations
+app.get('/api/test-data', testController.getDataFromTestCollection); // Route for fetching test data
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => {
     console.error('Could not connect to MongoDB:', err);
-    process.exit(1); // Optionally exit the process if the database connection fails
+    process.exit(1); // Exit the process on database connection failure
   });
 
-// Serve static files from the React frontend app
+// Serve static files (React frontend)
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-// Root route
+// Catch-all handler for React routing: serves index.html for all non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// Root route (serves the React app)
 app.get('/', (req, res) => {
   res.send('Welcome to TerraCanvas!');
 });
@@ -59,18 +64,11 @@ app.get('/', (req, res) => {
 // Global error handler
 app.use((err, req, res, next) => {
   console.error("Error:", err.message);
-
-  // Set the status code based on the error type
-  const statusCode = err.statusCode || 500;
-  res.status(statusCode);
-
-  // Send a detailed error message in development, simpler message in production
-  const response = {
+  const statusCode = err.statusCode || 500; // Default to 500 if status code not set
+  res.status(statusCode).json({
     message: process.env.NODE_ENV === 'development' ? err.message : 'An error occurred',
     ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
-  };
-
-  res.json(response);
+  });
 });
 
 // Start the server
@@ -80,5 +78,5 @@ if (require.main === module) {
   });
 }
 
-// Export the Express app for testing
+// Export the Express app for testing purposes
 module.exports = app;
