8b7f23315ba730da70b94e29cdf7ad421332bcb5
JuyeeMyeong <juyee.ming@gmail.com>
2023-11-29 09:15:17 -0500

    #3 Load File section (complete) 413 err

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
 // Load environment variables from .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // Import required modules
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 // Import route handlers
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const communityRoutes = require('./routes/communityRoutes');
-const postRoutes = require('./routes/postRoutes');
-const mapRoutes = require('./routes/mapRoutes');
-const testController = require('./controllers/testController');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const communityRoutes = require("./routes/communityRoutes");
+const postRoutes = require("./routes/postRoutes");
+const mapRoutes = require("./routes/mapRoutes");
+const testController = require("./controllers/testController");
 
 // Initialize Express application
 const app = express();
@@ -23,44 +23,47 @@ const PORT = process.env.PORT || 8080;
 
 // Apply middleware
 app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
-app.use(express.json()); // Parse incoming JSON payloads
+app.use(express.json({ limit: "50mb" })); // Parse incoming JSON payloads
 
 // Configure session management
-app.use(session({
-  secret: process.env.SESSION_SECRET, // Secret key for signing the session ID cookie
-  resave: false, // Avoid resaving sessions that haven't been modified
-  saveUninitialized: false, // Don't create session until something is stored
-  store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }), // Use MongoDB for session storage
-  cookie: { secure: process.env.NODE_ENV === 'production' }, // Use secure cookies in production
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // Secret key for signing the session ID cookie
+    resave: false, // Avoid resaving sessions that haven't been modified
+    saveUninitialized: false, // Don't create session until something is stored
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }), // Use MongoDB for session storage
+    cookie: { secure: process.env.NODE_ENV === "production" }, // Use secure cookies in production
+  })
+);
 
 // Define routes
-app.use('/auth', authRoutes); // Routes for authentication
-app.use('/api', postRoutes); // Routes for post-related operations
-app.use('/api/users', userRoutes); // Routes for user-related operations
-app.use('/api/community', communityRoutes); // Routes for community-related operations
-app.use('/api/mapgraphics', mapRoutes); // Routes for map graphics
-app.get('/api/test-data', testController.getDataFromTestCollection); // Route for fetching test data
+app.use("/auth", authRoutes); // Routes for authentication
+app.use("/api", postRoutes); // Routes for post-related operations
+app.use("/api/users", userRoutes); // Routes for user-related operations
+app.use("/api/community", communityRoutes); // Routes for community-related operations
+app.use("/api/mapgraphics", mapRoutes); // Routes for map graphics
+app.get("/api/test-data", testController.getDataFromTestCollection); // Route for fetching test data
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => {
-    console.error('Could not connect to MongoDB:', err);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.error("Could not connect to MongoDB:", err);
     process.exit(1); // Exit the process on database connection failure
   });
 
 // Serve static files (React frontend)
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 // Catch-all handler for React routing: serves index.html for all non-API routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // Root route (serves the React app)
-app.get('/', (req, res) => {
-  res.send('Welcome to TerraCanvas!');
+app.get("/", (req, res) => {
+  res.send("Welcome to TerraCanvas!");
 });
 
 // Global error handler
@@ -68,8 +71,11 @@ app.use((err, req, res, next) => {
   console.error("Error:", err.message);
   const statusCode = err.statusCode || 500; // Default to 500 if status code not set
   res.status(statusCode).json({
-    message: process.env.NODE_ENV === 'development' ? err.message : 'An error occurred',
-    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+    message:
+      process.env.NODE_ENV === "development"
+        ? err.message
+        : "An error occurred",
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
   });
 });
 
