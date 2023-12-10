8762ab742be5e99de87be04cf21f00923cc30ce9
BallisticDemon <albert.guan@stonybrook.edu>
2023-12-10 15:56:15 -0500

    sixty five cypress test cases done

--- cypress/e2e/community.cy.js
+++ cypress/e2e/community.cy.js
@@ -4,8 +4,43 @@ describe("Community Page", () => {
   });
 
 
-  it("should display the select bar correctly", () => {
+  it("should load the select bar correctly", () => {
     cy.get("[data-cy=community-select-bar]").should("exist");
+  });
+  it("should load the All word properly", () => {
+    cy.contains("All").should("exist");
+  });
+  it("should load the trending map graphics for all posts correctly", () => {
+    cy.get("[data-cy=all-posts-trending]").should("exist");
+  });
+  it("should load the questions for all posts correctly", () => {
+    cy.get("[data-cy=all-posts-questions]").should("exist");
+  });
+  it("should load the ideas for all posts correctly", () => {
+    cy.get("[data-cy=all-posts-ideas]").should("exist");
+  });
+  it("should load the make post button correctly", () => {
+    cy.get("[data-cy=community-make-post-button]").should("exist");
+  });
+  it("should have the make post button as disabled", () => {
+    cy.get("[data-cy=community-make-post-button]").should("be.disabled");
+  });
+  it("should load the questions posts once Questions is selected", () => {
+    cy.get("[data-cy=community-select-bar]").find("[data-cy=community-selector-questions]").should("exist");
+  });
+  it("should load the ideas posts once Ideas is selected", () => {
+    cy.get("[data-cy=community-select-bar]").find("[data-cy=community-selector-ideas]").should("exist");
+  });
+  it("should load the questions posts once Questions is selected", () => {
+    cy.get("[data-cy=community-select-bar]").find("[data-cy=community-selector-questions]").click();
+    cy.wait(2000);
+    cy.contains("Map Ideas").should("exist");
+  });
+  it("should load the ideas posts once Ideas is selected", () => {
+    cy.get("[data-cy=community-select-bar]").find("[data-cy=community-selector-ideas]").click();
+    cy.wait(2000);
+    cy.contains("Questions").should("exist");
+
   });
   
 
