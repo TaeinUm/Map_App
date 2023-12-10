4f2af562a6c98f09a2feeca48444bac31a8cc411
YUNBLAK <87653966+YUNBLAK@users.noreply.github.com>
2023-12-10 12:17:56 -0500

    #3 OTP Design

--- client/src/components/auth/ResetPassword.jsx
+++ client/src/components/auth/ResetPassword.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useContext } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import profileAPI from '../../api/profileAPI';
 import { PasswordRecoveryContext } from '../../contexts/PasswordRecoveryContext';
 
 const NewPasswordPage = () => {
-  const {updateUserPassword} = profileAPI;
+  const { updateUserPassword } = profileAPI;
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const {email} = useContext(PasswordRecoveryContext);
+  const { email } = useContext(PasswordRecoveryContext);
 
   const handleChangePassword = (event) => {
     setPassword(event.target.value);
@@ -19,37 +23,52 @@ const NewPasswordPage = () => {
   };
 
   const handleSubmit = () => {
-    // Implement your password update logic here
-    // For this example, you can add validation and update the password on the server
     updateUserPassword(email, confirmPassword);
     alert(`Password updated: ${password}`);
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h2>Set a New Password</h2>
-      <form>
-        <TextField
-          label="New Password"
-          variant="outlined"
-          type="password"
-          value={password}
-          onChange={handleChangePassword}
-        />
-        <br />
-        <TextField
-          label="Confirm Password"
-          variant="outlined"
-          type="password"
-          value={confirmPassword}
-          onChange={handleChangeConfirmPassword}
-        />
-        <br />
-        <Button variant="contained" color="primary" onClick={handleSubmit}>
-          Update Password
-        </Button>
-      </form>
-    </div>
+    <Container maxWidth="xs" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="h2" style={{ marginBottom: '20px' }}>
+            Set a New Password
+          </Typography>
+          <form>
+            <TextField
+              label="New Password"
+              variant="outlined"
+              type="password"
+              value={password}
+              onChange={handleChangePassword}
+              fullWidth
+              style={{ marginBottom: '20px' }}
+            />
+            <TextField
+              label="Confirm Password"
+              variant="outlined"
+              type="password"
+              value={confirmPassword}
+              onChange={handleChangeConfirmPassword}
+              fullWidth
+              style={{ marginBottom: '20px' }}
+            />
+            <Button 
+              variant="contained" 
+              color="primary" 
+              onClick={handleSubmit}
+              fullWidth
+              style={{
+                backgroundColor: 'black',
+                color: 'white',
+              }}
+            >
+              Update Password
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </Container>
   );
 };
 
