d4f7b6c6c36af0f0f9aed3cb9ef2821825da1544
YUNBLAK <87653966+YUNBLAK@users.noreply.github.com>
2023-12-15 19:58:15 -0500

    #3 Post Like Updating

--- client/src/components/communityTwo/PaginatedPosts.jsx
+++ client/src/components/communityTwo/PaginatedPosts.jsx
@@ -29,60 +29,68 @@ const PaginatedPosts = ({ posts, page, setPage, itemsPerPage }) => {
 
   return (
     <Box
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      margin: "20px",
+      marginBottom: "50px",
+    }}
+  >
+    <Grid
+      container
+      spacing={2}
+      justifyContent="center"
       sx={{
         display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        margin: "20px",
-        marginBottom: "50px",
+        justifyContent: "space-around",
       }}
     >
-      {/* <Typography
-        sx={{ color: "#fafafa", fontSize: "38px", marginBottom: "20px" }}
-      >
-        Title Here
-      </Typography> */}
-
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        sx={{
-          display: "flex",
-          justifyContent: "space-around",
-        }}
-      >
-        {paginatedPosts.map((post) => (
-          <Grid item xs={12} sm={6} md={4} key={post._id}>
-            <Card
+      {paginatedPosts.map((post) => (
+        <Grid item xs={12} sm={6} md={4} key={post._id}>
+          <Card
+            sx={{
+              margin: "10px",
+              height: "400px",
+              width: "320px",
+            }}
+            onClick={() => handlePostClick(post)}
+          >
+            <CardMedia
+              component="img"
+              height="200"
+              image={post.postImages || "https://via.placeholder.com/140"}
+              alt={post.postName}
+            />
+            <CardContent sx={{ textAlign: 'left' }}> 
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              noWrap 
               sx={{
-                margin: "10px",
-                height: "350px",
-                width: "320px",
+                width: '100%', 
+                overflow: 'hidden',
+                textOverflow: 'ellipsis', 
+                whiteSpace: 'nowrap' 
               }}
-              onClick={() => handlePostClick(post)}
             >
-              <CardMedia
-                component="img"
-                height="200"
-                image={post.postImages || "https://via.placeholder.com/140"}
-                alt={post.postName}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5">
-                  {post.postName}
-                </Typography>
-                <Typography variant="overline" display="block" gutterBottom>
-                  Author: {post.userId || "here! get author name"}
-                </Typography>
-                <Typography variant="overline" display="block" gutterBottom>
-                  Date: {new Date(post.postDate).toLocaleDateString()}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+              {post.postName}
+            </Typography>
+              <Typography variant="overline" display="block" gutterBottom  sx ={{marginBottom: "0px"}}>
+                Author: {post.userId || "Author name here"}
+              </Typography>
+              <Typography variant="overline" display="block" gutterBottom sx ={{marginBottom: "0px"}}>
+                Date: {new Date(post.postDate).toLocaleDateString()}
+              </Typography>
+              <Typography variant="overline" display="block" gutterBottom sx ={{marginBottom: "0px"}}>
+                Like: {post.interactions}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
       <Pagination
         count={Math.ceil(posts.length / itemsPerPage)}
         page={page}
