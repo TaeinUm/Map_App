c294304342e5c5d8e7b379e1254d34b2b1eb35ed
Taein Um <taein.um@stonybrook.edu>
2023-12-16 02:26:08 -0500

    #2 Header menu lighting persistent

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import {
   AppBar,
   Box,
@@ -57,6 +57,22 @@ function Header() {
     { title: "Sign Up", path: "/signup" },
   ];
 
+  const location = useLocation();
+
+  // Function to check if the current path matches or starts with a given path
+  const isActiveRoute = (path) => {
+    if (path === "/") {
+      // For the "Home" link, check if the path is exactly "/"
+      return location.pathname === path;
+    } else if (path === "/community") {
+      // For the "Community" link, check if the path starts with "/community" or "/posts"
+      return location.pathname.startsWith(path) || location.pathname.startsWith("/posts");
+    } else {
+      // For other links, check if the current path starts with the given path
+      return location.pathname.startsWith(path);
+    }
+  };  
+
   return (
     <AppBar
       position="static"
@@ -108,7 +124,7 @@ function Header() {
                 component={NavLink}
                 to={path}
                 style={({ isActive }) =>
-                  isActive ? { fontWeight: "bold", color: "white" } : undefined
+                isActive || isActiveRoute(path) ? { fontWeight: "bold", color: "white" } : undefined
                 }
               >
                 {title}
